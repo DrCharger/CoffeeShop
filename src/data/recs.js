021417078c6054a53f293db7162392944715342e
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-12-01 19:45:03 +0200

    qweqwe

--- src/data/recs.js
+++ src/data/recs.js
@@ -2,6 +2,13 @@ import Cappuchino from '../img/Capuchino.png';
 import mochiato from '../img/Machiato.png';
 import americano from '../img/Americano.png';
 import espresso from '../img/espresso.png';
+import avocado from '../img/avocado.jpg';
+import salad from '../img/summer.jpg';
+import choko from '../img/Choko.jpg';
+import caramMok from '../img/caramMok.jpg';
+import frap from '../img/Frap.jpg';
+import mocco from '../img/mocco.jpg';
+import strawberry from '../img/strawbery.jpg';
 
 export const recomended = [
   {
@@ -34,6 +41,18 @@ export const allList = [
         text: 'Caramel Machiato',
         price: '$ 3.00',
       },
+      {
+        id: 'hot-03',
+        img: espresso,
+        text: 'Espresso',
+        price: '$ 2.00',
+      },
+      {
+        id: 'hot-04',
+        img: americano,
+        text: 'Americano',
+        price: '$ 4.00',
+      },
     ],
   },
   {
@@ -41,27 +60,50 @@ export const allList = [
     prods: [
       {
         id: 'cold-01',
-        img: espresso,
-        text: 'Espresso',
-        price: '$ 2.00',
-      },
-      {
-        id: 'cold-02',
-        img: americano,
-        text: 'Americano',
-        price: '$ 4.00',
-      },
-      {
-        id: 'cold-03',
-        img: Cappuchino,
-        text: 'Cappuchino Short',
+        img: choko,
+        text: 'Choko Coffe',
         price: '$ 5.00',
       },
       {
-        id: 'cold-04',
-        img: mochiato,
-        text: 'Caramel Machiato',
+        id: 'cold-02',
+        img: strawberry,
+        text: 'StrawBerry Coffee',
         price: '$ 3.00',
+      },
+      {
+        id: 'cold-03',
+        img: caramMok,
+        text: 'Caramel Coffee',
+        price: '$ 3.00',
+      },
+      {
+        id: 'cold-04',
+        img: frap,
+        text: 'Frapuchino Coffee',
+        price: '$ 3.00',
+      },
+      {
+        id: 'cold-05',
+        img: mocco,
+        text: 'Moccochino Coffee',
+        price: '$ 3.00',
+      },
+    ],
+  },
+  {
+    id: '03',
+    prods: [
+      {
+        id: 'salad-01',
+        img: avocado,
+        text: 'Avocado Salad',
+        price: '$ 10.00',
+      },
+      {
+        id: 'salad-02',
+        img: salad,
+        text: 'Summer Salad',
+        price: '$ 6.00',
       },
     ],
   },
