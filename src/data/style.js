911baa7e35ebea32b22f93e15cfb15afbc68d632
Dr.Charger <dmitriy.nasa@gmail.com>
2022-12-26 13:24:20 +0200

    1

--- src/data/style.js
+++ src/data/style.js
@@ -2,15 +2,17 @@ export const style = {
   width: '90vw',
   borderRadius: '10px',
   position: 'fixed',
-  top: '90vh',
+  top: '90%',
   left: '5vw',
   backgroundColor: '#543820',
   opacity: 1,
   zIndex: 999,
+  display: 'flex',
+  alignItems: 'center',
 };
 
 export const styleAction = {
   color: '#fff',
   minWidth: '0',
-  padding: '14px 0 0',
+  padding: '10px 0 0',
 };
